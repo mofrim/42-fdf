2051094e09c4e62ba43c8f3b2a119b68260610d7
mofrim <mofrim@posteo.de>
2024-09-21 15:04:50 +0200

    rename iso_proj -> general_proj

--- general_projection.c
+++ general_projection.c
@@ -1,39 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   isometric_projection.c                             :+:      :+:    :+:   */
+/*   general_projection.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 23:38:01 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/09/21 12:59:25 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/09/21 14:39:56 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-// void isometric_proj(t_map *map)
-// {
-// 	double isoproj_mat[3][3];
-// 	double	one_by_sqrt6;
-//
-// 	one_by_sqrt6 = 1 / sqrt(6);
-// 	isoproj_mat[0][0] = one_by_sqrt6 * sqrt(3);
-// 	isoproj_mat[0][1] = 0;
-// 	isoproj_mat[0][2] = one_by_sqrt6 * (-sqrt(3));
-// 	isoproj_mat[1][0] = one_by_sqrt6;
-// 	isoproj_mat[1][1] = -one_by_sqrt6 * 2;
-// 	isoproj_mat[1][2] = one_by_sqrt6 * 1;
-// 	isoproj_mat[2][0] = 0;
-// 	isoproj_mat[2][1] = 0;
-// 	isoproj_mat[2][2] = 0;
-// 	mult_mat_map(isoproj_mat, map);
-// }
-
 /* custom proj.
  *
  * normally it is: rotate about 45deg around the vertical (y?! z?!) axis then
  * atan(1/sqrt(2)) about the x axis
+ *
+ * FIXME remove unneccessary double pointer
+ *
+ * TODO i have to do the already existing rotations first!!! and then the new
+ * one!!! omg! how do i keep track of what was done so far?!?! and in which
+ * order?!?
+ * O - M - G!!! do i really have to keep track of all rotations been done using a
+ * linked list ?!?
  */
 void general_proj(t_myxvar **mxv, double alpha, double beta, double gamma)
 {

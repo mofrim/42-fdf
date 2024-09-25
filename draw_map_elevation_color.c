012c359d7c340acb0ad00947351f6900a52bcb1f
mofrim <mofrim@posteo.de>
2024-09-25 21:31:04 +0200

    harden draw_map_elev against evil maps

--- draw_map_elevation_color.c
+++ draw_map_elevation_color.c
@@ -6,7 +6,7 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 11:53:59 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/09/25 20:53:47 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/09/25 21:29:38 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,39 +90,55 @@ void	draw_map_color_elev(t_map *map, t_myxvar mxv)
 		j = -1;
 		while (++j < map->cols)
 		{
-			/* first line. */
-			if (i == 0 && j == 0)
+			/* one liner map. */
+			if (map->rows == 1 && map->cols > 1)
 			{
-				draw_color_line_elev(vmap[0][0], vmap[1][0], mxv);
-				draw_color_line_elev(vmap[0][0], vmap[0][1], mxv);
+				if (0 <= j && j < map->cols - 1)
+					draw_color_line_elev(vmap[0][j], vmap[0][j + 1], mxv);
 			}
-			else if (i == 0 && 0 < j && j < map->cols - 1)
+			else if (map->cols == 1 && map->rows > 1)
 			{
-				draw_color_line_elev(vmap[0][j], vmap[1][j], mxv);
-				draw_color_line_elev(vmap[0][j], vmap[0][j + 1], mxv);
+				if (0 <= i && i < map->rows - 1)
+					draw_color_line_elev(vmap[i][0], vmap[i + 1][0], mxv);
 			}
-			else if (i == 0 && j == map->cols - 1)
-				draw_color_line_elev(vmap[0][j], vmap[1][j], mxv);
+			else if (map->rows == 1 && map->cols == 1)
+				return ;
+			else
+			{
+				/* first line. */
+				if (i == 0 && j == 0)
+				{
+					draw_color_line_elev(vmap[0][0], vmap[1][0], mxv);
+					draw_color_line_elev(vmap[0][0], vmap[0][1], mxv);
+				}
+				else if (i == 0 && 0 < j && j < map->cols - 1)
+				{
+					draw_color_line_elev(vmap[0][j], vmap[1][j], mxv);
+					draw_color_line_elev(vmap[0][j], vmap[0][j + 1], mxv);
+				}
+				else if (i == 0 && j == map->cols - 1)
+					draw_color_line_elev(vmap[0][j], vmap[1][j], mxv);
 
-			/* in between. */
-			if (i != 0 && i < map->rows - 1 && j == 0)
-			{
-				draw_color_line_elev(vmap[i][j], vmap[i][j + 1], mxv);
-				draw_color_line_elev(vmap[i][j], vmap[i + 1][j], mxv);
-			}
-			else if (i != 0 && i < map->rows - 1 && 0 < j && j < map->cols - 1)
-			{
-				draw_color_line_elev(vmap[i][j], vmap[i][j + 1], mxv);
-				draw_color_line_elev(vmap[i][j], vmap[i + 1][j], mxv);
-			}
-			else if (i != 0 && i < map->rows - 1 && j == map->cols - 1)
-				draw_color_line_elev(vmap[i][j], vmap[i + 1][j], mxv);
+				/* in between. */
+				if (i != 0 && i < map->rows - 1 && j == 0)
+				{
+					draw_color_line_elev(vmap[i][j], vmap[i][j + 1], mxv);
+					draw_color_line_elev(vmap[i][j], vmap[i + 1][j], mxv);
+				}
+				else if (i != 0 && i < map->rows - 1 && 0 < j && j < map->cols - 1)
+				{
+					draw_color_line_elev(vmap[i][j], vmap[i][j + 1], mxv);
+					draw_color_line_elev(vmap[i][j], vmap[i + 1][j], mxv);
+				}
+				else if (i != 0 && i < map->rows - 1 && j == map->cols - 1)
+					draw_color_line_elev(vmap[i][j], vmap[i + 1][j], mxv);
 
-			/* last line. */
-			if (i == map->rows - 1 && j == 0)
-				draw_color_line_elev(vmap[i][j], vmap[i][j + 1], mxv);
-			else if (i == map->rows - 1 && 0 < j && j < map->cols - 1)
-				draw_color_line_elev(vmap[i][j], vmap[i][j + 1], mxv);
+				/* last line. */
+				if (i == map->rows - 1 && j == 0)
+					draw_color_line_elev(vmap[i][j], vmap[i][j + 1], mxv);
+				else if (i == map->rows - 1 && 0 < j && j < map->cols - 1)
+					draw_color_line_elev(vmap[i][j], vmap[i][j + 1], mxv);
+			}
 		}
 	}
 }

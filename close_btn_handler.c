dd78a3d5c68accddcb06284c7b1cbdf98d2758c4
fmaurer <fmaurer@school>
2024-09-27 14:28:10 +0200

    add close_btn_handler

---
+++ close_btn_handler.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   close_btn_handler.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/27 14:23:54 by fmaurer           #+#    #+#             */
+/*   Updated: 2024/09/27 14:26:59 by fmaurer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+/* Cleanly exit the program when close button is clicked. */
+int	close_btn_handler(t_myxvar *mx)
+{
+	ft_printf("Close button clicked. Exiting.\n");
+	mlx_destroy_window(mx->mlx, mx->win);
+	if (mx->orig_map == mx->cur_map)
+	{
+		free_map(&mx->orig_map);
+		mx->cur_map = NULL;
+	}
+	else
+		free_map(&mx->orig_map);
+	if (mx->cur_map)
+		free_map(&mx->cur_map);
+	free(mx->colrmap);
+	mlx_destroy_display(mx->mlx);
+	free(mx->mlx);
+	free(mx);
+	exit(0);
+}

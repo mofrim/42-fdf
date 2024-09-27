4f16118bb29966e353bebb9d69ebd4d6b5b01005
mofrim <mofrim@posteo.de>
2024-09-27 11:27:31 +0200

    refac sidebar

--- sidebar.c
+++ sidebar.c
@@ -6,74 +6,72 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 16:40:42 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/09/25 20:35:55 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/09/27 11:19:00 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include "libft/libft.h"
 
-void	print_menu_text(t_myxvar *mx, int x, int y, char *txt)
+static void	print_menu_text(t_myxvar *mx, int x, int y, char *txt);
+static void	show_keys(t_myxvar *mx, int *i);
+static void	show_mapinfo(t_myxvar *mx, int *i);
+static void	print_mapinfo(t_myxvar *mx, const char *txt, int prop, int *i);
+
+void	show_sidebar(t_myxvar *mx)
+{
+	int		i;
+
+	i = 1;
+	show_keys(mx, &i);
+	i += 2;
+	show_mapinfo(mx, &i);
+}
+
+static void	print_menu_text(t_myxvar *mx, int x, int y, char *txt)
 {
 	mlx_string_put(mx->mlx, mx->win, x, y, rgb_to_int("aaaaaa"), txt);
 }
 
-void	show_sidebar(t_myxvar *mxv)
+static void	show_keys(t_myxvar *mx, int *i)
 {
-	int	i;
-	char	*txt;
-	char	*prop;
+	print_menu_text(mx, 15, 20, "-- Shortcuts --");
+	print_menu_text(mx, 15, 20 + (++(*i)) * 15, "arrows = move map");
+	print_menu_text(mx, 15, 20 + (++(*i)) * 15, ",/.    = scale height");
+	print_menu_text(mx, 15, 20 + (++(*i)) * 15, "+/-    = zoom in/out");
+	print_menu_text(mx, 15, 20 + (++(*i)) * 15, "q/a    = +/- rot_x");
+	print_menu_text(mx, 15, 20 + (++(*i)) * 15, "w/s    = +/- rot_y");
+	print_menu_text(mx, 15, 20 + (++(*i)) * 15, "e/d    = +/- rot_z");
+	print_menu_text(mx, 15, 20 + (++(*i)) * 15, "i      = draw iso proj");
+	print_menu_text(mx, 15, 20 + (++(*i)) * 15, "1,2,3  = change linestyle");
+	print_menu_text(mx, 15, 20 + (++(*i)) * 15, "m      = toggle markers");
+	print_menu_text(mx, 15, 20 + (++(*i)) * 15, "n/b    = +/- markersize");
+	print_menu_text(mx, 15, 20 + (++(*i)) * 15, "c      = toggle autocenter");
+	print_menu_text(mx, 15, 20 + (++(*i)) * 15, "r      = reset proj");
+	print_menu_text(mx, 15, 20 + (++(*i)) * 15, "q      = quit program");
+}
 
-	i = 1;
-	print_menu_text(mxv, 15, 20, "-- Shortcuts --");
-	print_menu_text(mxv, 15, 20 + (++i)*15, "arrows = move map");
-	print_menu_text(mxv, 15, 20 + (++i)*15, ",/.    = scale height");
-	print_menu_text(mxv, 15, 20 + (++i)*15, "+/-    = zoom in/out");
-	print_menu_text(mxv, 15, 20 + (++i)*15, "q/a    = +/- rot_x");
-	print_menu_text(mxv, 15, 20 + (++i)*15, "w/s    = +/- rot_y");
-	print_menu_text(mxv, 15, 20 + (++i)*15, "e/d    = +/- rot_z");
-	print_menu_text(mxv, 15, 20 + (++i)*15, "i      = draw iso proj");
-	print_menu_text(mxv, 15, 20 + (++i)*15, "r      = reset proj");
-	print_menu_text(mxv, 15, 20 + (++i)*15, "c      = toggle autocenter");
-	print_menu_text(mxv, 15, 20 + (++i)*15, "m      = toggle markers");
-	print_menu_text(mxv, 15, 20 + (++i)*15, "q      = quit program");
-	print_menu_text(mxv, 15, 20 + (++i)*15, "q      = quit program");
-	i += 2;
-	print_menu_text(mxv, 15, 20 + (++i)*15, "-- Map Info --");
-	i++;
-	prop = ft_itoa(mxv->orig_map->rows);
-	txt = ft_strjoin("map rows: ", prop);
-	print_menu_text(mxv, 15, 20 + (++i)*15, txt);
-	free(txt);
-	free(prop);
-	prop = ft_itoa(mxv->orig_map->cols);
-	txt = ft_strjoin("map cols: ", prop);
-	print_menu_text(mxv, 15, 20 + (++i)*15, txt);
-	free(txt);
-	free(prop);
-	prop = ft_itoa(mxv->auto_center_map);
-	txt = ft_strjoin("auto_center: ", prop);
-	print_menu_text(mxv, 15, 20 + (++i)*15, txt);
-	free(txt);
-	free(prop);
-	prop = ft_itoa((int)mxv->zfac);
-	txt = ft_strjoin("zfac: ", prop);
-	print_menu_text(mxv, 15, 20 + (++i)*15, txt);
-	free(txt);
-	free(prop);
-	prop = ft_itoa((int)mxv->xyfac);
-	txt = ft_strjoin("xyfac: ", prop);
-	print_menu_text(mxv, 15, 20 + (++i)*15, txt);
-	free(txt);
-	free(prop);
-	prop = ft_itoa((int)mxv->zmax);
-	txt = ft_strjoin("zmax: ", prop);
-	print_menu_text(mxv, 15, 20 + (++i)*15, txt);
-	free(txt);
-	free(prop);
-	prop = ft_itoa((int)mxv->zdiff);
-	txt = ft_strjoin("zdiff: ", prop);
-	print_menu_text(mxv, 15, 20 + (++i)*15, txt);
-	free(txt);
-	free(prop);
+static void	show_mapinfo(t_myxvar *mx, int *i)
+{
+	print_menu_text(mx, 15, 20 + (++(*i)) * 15, "-- Map Info --");
+	(*i)++;
+	print_mapinfo(mx, "map rows: ", mx->orig_map->rows, i);
+	print_mapinfo(mx, "map cols: ", mx->orig_map->cols, i);
+	print_mapinfo(mx, "auto_center: ", mx->auto_center_map, i);
+	print_mapinfo(mx, "zfac: ", (int)mx->zfac, i);
+	print_mapinfo(mx, "zmax: ", (int)mx->zmax, i);
+	print_mapinfo(mx, "zmin: ", (int)mx->zmin, i);
+	print_mapinfo(mx, "zdiff: ", (int)mx->zdiff, i);
+}
+
+static void	print_mapinfo(t_myxvar *mx, const char *txt, int prop, int *i)
+{
+	char	*prop_str;
+	char	*msg;
+
+	prop_str = ft_itoa(prop);
+	msg = ft_strjoin(txt, prop_str);
+	print_menu_text(mx, 15, 20 + (++(*i)) * 15, msg);
+	free(msg);
+	free(prop_str);
 }

06ef65f63ab11083605b77fb790e1aadafd90e7e
mofrim <mofrim@posteo.de>
2024-09-30 11:01:52 +0200

    some renaming

--- gen_colormap.c
+++ gen_colormap.c
@@ -6,7 +6,7 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 14:04:24 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/09/27 15:34:09 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/09/30 07:25:57 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,25 @@
  * 0 -> diff/2 gen colrs between 0000ff and 00ff00 
  * diff/2 -> diff gen colrs between 00ff00 and ff0000
  */
-int	*generate_colrmap(t_myxvar mxv)
+int	*generate_colrmap(t_myxvar mx)
 {
 	int	*cmap;
 	int	i;
 	int	n;
 
-	cmap = malloc(sizeof(int) * (mxv.zdiff + 1));
-	if (mxv.zdiff == 0)
+	cmap = malloc(sizeof(int) * (mx.zdiff + 1));
+	if (mx.zdiff == 0)
 	{
 		cmap[0] = rgb_to_int("aaaaaa");
 		return (cmap);
 	}
 	i = -1;
-	n = mxv.zdiff / 2;
+	n = mx.zdiff / 2;
 	while (++i <= n)
 		cmap[i] = (((int)(255 * (double)i / n)) << 8) | ((int)(255 * (1.0 - \
 						(double)i / n)));
 	i = -1;
-	while (++i + n + 1 <= mxv.zdiff)
+	while (++i + n + 1 <= mx.zdiff)
 		cmap[n + 1 + i] = (((int)(255 * (double)i / n)) << 16) | (((int)(255 * \
 						(1.0 - (double)i / n))) << 8);
 	return (cmap);

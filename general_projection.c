2a7d6076942f0a6b99978fdc01deb8bc8faf7ff9
mofrim <mofrim@posteo.de>
2024-09-30 06:41:16 +0200

    pre reprogram general proj to be in-place

--- general_projection.c
+++ general_projection.c
@@ -6,7 +6,7 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 23:38:01 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/09/27 08:58:29 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/09/29 22:50:43 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ static void	reset_zvalues(t_map *cur_map, t_map *orig_map);
  *
  * Isometric projection would be: rotate about 45deg around the vertical (y?!
  * z?!) axis then atan(1/sqrt(2)) about the x axis.
+ *
+ * TODO switch back to in place multiplication an observe shrinking
  */
 void	general_proj(t_myxvar *mx, double alpha, double beta, double gamma)
 {
@@ -41,6 +43,27 @@ void	general_proj(t_myxvar *mx, double alpha, double beta, double gamma)
 	rot_map_x(mx->cur_map, alpha);
 	rot_map_y(mx->cur_map, beta);
 	rot_map_z(mx->cur_map, gamma);
+	trans_zoom_map(mx->cur_map, mpbak.old_zoom, mpbak.old_xoff, \
+			mpbak.old_yoff);
+	reset_zvalues(mx->cur_map, mx->orig_map);
+}
+
+void	iso_proj(t_myxvar *mx)
+{
+	t_map_props_bak	mpbak;
+
+	mpbak.old_zoom = mx->cur_map->zoom;
+	mpbak.old_xoff = mx->cur_map->x_offset;
+	mpbak.old_yoff = mx->cur_map->y_offset;
+	mpbak.old_alpha = mx->cur_map->alpha;
+	mpbak.old_beta = mx->cur_map->beta;
+	mpbak.old_gamma = mx->cur_map->gamma;
+	free_map(&mx->cur_map);
+	mx->cur_map = duplicate_map(mx->orig_map);
+	mx->cur_map->alpha = mpbak.old_alpha;
+	mx->cur_map->beta = mpbak.old_beta;
+	mx->cur_map->gamma = mpbak.old_gamma;
+	rot_map_iso(mx->cur_map);
 	trans_zoom_map(mx->cur_map, mpbak.old_zoom, mpbak.old_xoff, \
 			mpbak.old_yoff);
 	reset_zvalues(mx->cur_map, mx->orig_map);

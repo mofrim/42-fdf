651dbb7ccda39bea28322e59e339c749fd278c00
mofrim <mofrim@posteo.de>
2024-09-30 11:06:33 +0200

    remove obsolete reset_zvalues(), cleanup general_projection

--- general_projection.c
+++ general_projection.c
@@ -6,25 +6,37 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 23:38:01 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/09/30 10:45:13 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/09/30 11:06:20 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-// static void	reset_zvalues(t_map *cur_map, t_map *orig_map);
 
-/* Do general rotation by alpha around x, beta around y, gamma around z-axis.
- * The rotation will be applied to the numerically unspoiled orig_map. After
- * applying the rotations the map will be restored to its last position and zoom
- * status in the window. Finally all z-values will be (falsely) reset to their
- * orig_map values in order to get elevation-coloring right.
- *
- * Isometric projection would be: rotate about 45deg around the vertical (y?!
- * z?!) axis then atan(1/sqrt(2)) about the x axis.
- *
- * TODO switch back to in place multiplication an observe shrinking
- */
+/* Rotate cur_map about x, y, or z-axis. Only one axis rotation at a time! */
+// FIXME rename!
+void	general_proj(t_myxvar *mx, double alpha, double beta, double gamma)
+{
+	if (alpha)
+	{
+		rot_map_x(mx->cur_map, alpha);
+		anglst_add_back(&mx->anglst, anglstnew(alpha, 0, 0));
+	}
+	else if (beta)
+	{
+		rot_map_y(mx->cur_map, beta);
+		anglst_add_back(&mx->anglst, anglstnew(0, beta, 0));
+	}
+	else if (gamma)
+	{
+		rot_map_z(mx->cur_map, gamma);
+		anglst_add_back(&mx->anglst, anglstnew(0, 0, gamma));
+	}
+}
+
+
+//// Old version...
+//
 // void	general_proj(t_myxvar *mx, double alpha, double beta, double gamma)
 // {
 // 	t_map_props_bak	mpbak;
@@ -48,26 +60,8 @@
 // 	reset_zvalues(mx->cur_map, mx->orig_map);
 // }
 
-/* rewrite. */
-void	general_proj(t_myxvar *mx, double alpha, double beta, double gamma)
-{
-	if (alpha)
-	{
-		rot_map_x(mx->cur_map, alpha);
-		anglst_add_back(&mx->anglst, anglstnew(alpha, 0, 0));
-	}
-	else if (beta)
-	{
-		rot_map_y(mx->cur_map, beta);
-		anglst_add_back(&mx->anglst, anglstnew(0, beta, 0));
-	}
-	else if (gamma)
-	{
-		rot_map_z(mx->cur_map, gamma);
-		anglst_add_back(&mx->anglst, anglstnew(0, 0, gamma));
-	}
-}
-
+/* Generel Projection func used in replay_rotations() after a map height
+ * scaling. */
 void	general_proj_replay(t_myxvar *mx, double alpha, double beta, \
 		double gamma)
 {
@@ -98,18 +92,3 @@ void	iso_proj(t_myxvar *mx)
 	trans_zoom_map(mx->cur_map, mpbak.old_zoom, mpbak.old_xoff, \
 			mpbak.old_yoff);
 }
-
-/* Helper func for resetting z-values of cur_map to the ones from orig_map. */
-// static void	reset_zvalues(t_map *cur_map, t_map *orig_map)
-// {
-// 	int		i;
-// 	int		j;
-//
-// 	i = -1;
-// 	while (++i < cur_map->rows)
-// 	{
-// 		j = -1;
-// 		while (++j < cur_map->cols)
-// 			cur_map->vec_map[i][j].zo = orig_map->vec_map[i][j].z;
-// 	}
-// }

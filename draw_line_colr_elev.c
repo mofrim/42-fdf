6b0caf34cd92aed6571dcd1bde6336d4b162b46e
mofrim <mofrim@posteo.de>
2024-09-30 11:01:52 +0200

    some renaming

--- draw_line_colr_elev.c
+++ draw_line_colr_elev.c
@@ -6,21 +6,24 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 14:06:15 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/09/27 15:00:49 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/09/30 08:14:43 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+// FIXME
+#include <stdio.h>
+
 /* Get the pixel color from colormap stored in my xvar. */
-int	get_elev_colr(int z, t_myxvar mxv)
+int	get_elev_colr(int z, t_myxvar mx)
 {
-	return (mxv.colrmap[(int)(z / mxv.zfac) - mxv.zmin]);
+	return (mx.colrmap[(int)(z / mx.zfac) - mx.zmin]);
 }
 
 /* Draw a colored line using the z-values (wich have been reset to orig_map's)
  * and the colormap calculating on the initial z-values. */
-void	draw_line_colr_elev(t_vec a, t_vec b, t_myxvar mxv)
+void	draw_line_colr_elev(t_vec a, t_vec b, t_myxvar mx)
 {
 	double	dx;
 	double	dy;
@@ -36,15 +39,15 @@ void	draw_line_colr_elev(t_vec a, t_vec b, t_myxvar mxv)
 		n = fabs(dy);
 	dx = dx / (double)n;
 	dy = dy / (double)n;
-	dz = (b.z - a.z) / (double)n;
+	dz = (b.zo - a.zo) / (double)n;
 	i = 0;
 	while (i <= n)
 	{
 		if (MAP_AREA_MINX <= a.x && a.x <= WINX && 0 <= a.y && a.y <= WINY)
-			mlx_pixel_put(mxv.mlx, mxv.win, a.x, a.y, get_elev_colr(a.z, mxv));
+			mlx_pixel_put(mx.mlx, mx.win, a.x, a.y, get_elev_colr((int)a.zo, mx));
 		a.x += dx;
 		a.y += dy;
-		a.z += dz;
+		a.zo += dz;
 		i++;
 	}
 }
--- draw_map.c
+++ draw_map.c
@@ -6,7 +6,7 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 16:12:23 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/09/27 15:23:54 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/09/30 08:33:05 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,30 +24,30 @@ static void	draw_one_rowcol_maps(t_map *map, void (*line_draw_func)(t_vec, \
 
 /* Generic map drawing function. line_draw_func() is function used for drawing
  * the lines of the map. Allows for flexible styling. */
-void	draw_map(t_map *map, t_myxvar mxv, void (*line_draw_func)\
+void	draw_map(t_map *map, t_myxvar mx, void (*line_draw_func)\
 		(t_vec, t_vec, t_myxvar))
 {
 	int			i;
 
 	if (map->rows == 1 || map->cols == 1)
-		draw_one_rowcol_maps(map, line_draw_func, mxv);
+		draw_one_rowcol_maps(map, line_draw_func, mx);
 	else
 	{
 		i = -1;
 		while (++i < map->rows)
 		{
 			if (i == 0)
-				draw_first_mapline(map, line_draw_func, mxv);
+				draw_first_mapline(map, line_draw_func, mx);
 			else if (0 < i && i < map->rows - 1)
-				draw_amidst_mapline(map, i, line_draw_func, mxv);
+				draw_amidst_mapline(map, i, line_draw_func, mx);
 			else
-				draw_last_mapline(map, i, line_draw_func, mxv);
+				draw_last_mapline(map, i, line_draw_func, mx);
 		}
 	}
 }
 
 void	draw_one_rowcol_maps(t_map *map, void (*line_draw_func)(t_vec, t_vec, \
-			t_myxvar), t_myxvar mxv)
+			t_myxvar), t_myxvar mx)
 {
 	int		k;
 	t_vec	**vmap;
@@ -58,20 +58,20 @@ void	draw_one_rowcol_maps(t_map *map, void (*line_draw_func)(t_vec, t_vec, \
 	{
 		while (++k < map->cols)
 			if (0 <= k && k < map->cols - 1)
-				line_draw_func(vmap[0][k], vmap[0][k + 1], mxv);
+				line_draw_func(vmap[0][k], vmap[0][k + 1], mx);
 	}
 	else if (map->rows > 1 && map->cols == 1)
 	{
 		while (++k < map->rows)
 			if (0 <= k && k < map->rows - 1)
-				line_draw_func(vmap[k][0], vmap[k + 1][0], mxv);
+				line_draw_func(vmap[k][0], vmap[k + 1][0], mx);
 	}
 	else
 		return ;
 }
 
 void	draw_first_mapline(t_map *map, void (*line_draw_func)(t_vec, t_vec, \
-			t_myxvar), t_myxvar mxv)
+			t_myxvar), t_myxvar mx)
 {
 	int		j;
 	t_vec	**vmap;
@@ -82,21 +82,21 @@ void	draw_first_mapline(t_map *map, void (*line_draw_func)(t_vec, t_vec, \
 	{
 		if (j == 0)
 		{
-			line_draw_func(vmap[0][0], vmap[1][0], mxv);
-			line_draw_func(vmap[0][0], vmap[0][1], mxv);
+			line_draw_func(vmap[0][0], vmap[1][0], mx);
+			line_draw_func(vmap[0][0], vmap[0][1], mx);
 		}
 		else if (0 < j && j < map->cols - 1)
 		{
-			line_draw_func(vmap[0][j], vmap[1][j], mxv);
-			line_draw_func(vmap[0][j], vmap[0][j + 1], mxv);
+			line_draw_func(vmap[0][j], vmap[1][j], mx);
+			line_draw_func(vmap[0][j], vmap[0][j + 1], mx);
 		}
 		else if (j == map->cols - 1)
-			line_draw_func(vmap[0][j], vmap[1][j], mxv);
+			line_draw_func(vmap[0][j], vmap[1][j], mx);
 	}
 }
 
 void	draw_amidst_mapline(t_map *map, int i, void (*line_draw_func)(t_vec, \
-			t_vec, t_myxvar), t_myxvar mxv)
+			t_vec, t_myxvar), t_myxvar mx)
 {
 	int		j;
 	t_vec	**vmap;
@@ -107,21 +107,21 @@ void	draw_amidst_mapline(t_map *map, int i, void (*line_draw_func)(t_vec, \
 	{
 		if (j == 0)
 		{
-			line_draw_func(vmap[i][j], vmap[i][j + 1], mxv);
-			line_draw_func(vmap[i][j], vmap[i + 1][j], mxv);
+			line_draw_func(vmap[i][j], vmap[i][j + 1], mx);
+			line_draw_func(vmap[i][j], vmap[i + 1][j], mx);
 		}
 		else if (0 < j && j < map->cols - 1)
 		{
-			line_draw_func(vmap[i][j], vmap[i][j + 1], mxv);
-			line_draw_func(vmap[i][j], vmap[i + 1][j], mxv);
+			line_draw_func(vmap[i][j], vmap[i][j + 1], mx);
+			line_draw_func(vmap[i][j], vmap[i + 1][j], mx);
 		}
 		else
-			line_draw_func(vmap[i][j], vmap[i + 1][j], mxv);
+			line_draw_func(vmap[i][j], vmap[i + 1][j], mx);
 	}
 }
 
 void	draw_last_mapline(t_map *map, int i, void (*line_draw_func)(t_vec, \
-			t_vec, t_myxvar), t_myxvar mxv)
+			t_vec, t_myxvar), t_myxvar mx)
 {
 	int		j;
 	t_vec	**vmap;
@@ -131,8 +131,8 @@ void	draw_last_mapline(t_map *map, int i, void (*line_draw_func)(t_vec, \
 	while (++j < map->cols)
 	{
 		if (j == 0)
-			line_draw_func(vmap[i][j], vmap[i][j + 1], mxv);
+			line_draw_func(vmap[i][j], vmap[i][j + 1], mx);
 		else if (0 < j && j < map->cols - 1)
-			line_draw_func(vmap[i][j], vmap[i][j + 1], mxv);
+			line_draw_func(vmap[i][j], vmap[i][j + 1], mx);
 	}
 }
